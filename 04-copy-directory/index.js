2c401680064cda2adf9e7fd6ec2490ef5c819369
avtew <avtew@outlook.com>
2022-05-24 00:34:38 +0300

    feat: implement task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
-
+(function copyDir() {
+  const fs = require('fs/promises');
+  const path = require('path');
+  const src = path.join(__dirname, 'files');
+  const dest = path.join(__dirname, 'files-copy');
+  fs.mkdir(dest, { recursive: true });
+  fs.readdir(src, {withFileTypes: true}).then (files => {
+    for (let file of files) {
+      fs.copyFile(path.join(src, file.name), path.join(dest, file.name));
+    }
+  })
+}())
