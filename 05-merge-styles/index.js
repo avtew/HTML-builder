91dc29e120f3266bedd805de08e0b1ab85115abe
avtew <avtew@outlook.com>
2022-05-24 18:46:02 +0300

    feat: implement task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+(function merge() {
+  const fs = require('fs/promises');
+  const path = require('path');
+  const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+  fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}).then (files => {
+    for (let file of files) {
+      const dir = path.join(__dirname, 'styles', file.name);
+      if (path.extname(dir) === '.css') {
+        let arr = [];
+        arr.push(fs.readFile(dir, 'utf8'));
+        Promise.all(arr).then (data => {
+          fs.appendFile(bundle, data);
+        });
+      }
+    }
+  });
+}());
